e379d7b00ebf8d56b45dbc44803578d91403e2a8
Pichau <igorof06@outlook.com>
2025-02-09 21:37:59 -0300

    Starting Project

---
+++ notification-scheduler-api/src/main/java/com/igor/notification_scheduler_api/NotificationSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package com.igor.notification_scheduler_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationSchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationSchedulerApiApplication.class, args);
+	}
+
+}
---
+++ notification-scheduler-api/src/test/java/com/igor/notification_scheduler_api/NotificationSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igor.notification_scheduler_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationSchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
